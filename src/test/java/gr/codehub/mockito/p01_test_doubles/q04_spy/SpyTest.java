bd77f0aee67690a27e7c53bd4734f9f24b2cf629
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-04-12 12:04:21 +0300

    Spy complete

--- src/test/java/gr/codehub/mockito/p01_test_doubles/q04_spy/SpyTest.java
+++ src/test/java/gr/codehub/mockito/p01_test_doubles/q04_spy/SpyTest.java
@@ -6,22 +6,26 @@ import org.mockito.Mockito;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 public class SpyTest {
 
     @Test
     public void demoSpyWithMockito() {
-        PhoneRepositorySpy phoneRepositorySpy = new PhoneRepositorySpy();
+        PhoneRepository phoneRepositorySpy =  spy(PhoneRepository.class);
         PhoneService phoneService = new PhoneService(phoneRepositorySpy);
 
         Phone phone1 = new Phone("1234", "CodePhone Pro", 600, LocalDate.now());
         Phone phone2 = new Phone("1235", "CodePhone Standard", 500, LocalDate.now());
 
         phoneService.addPhone(phone1); // return
-        assertEquals(0, phoneRepositorySpy.timesCalled());
+        //assertEquals(0, phoneRepositorySpy.timesCalled());
+        verify(phoneRepositorySpy, Mockito.times(0)).save(phone1);
 
         phoneService.addPhone(phone2); // save will be called
-        assertEquals(1, phoneRepositorySpy.timesCalled());
+        verify(phoneRepositorySpy, Mockito.times(1)).save(phone2);
+        //assertEquals(1, phoneRepositorySpy.timesCalled());
     }
 
 }
